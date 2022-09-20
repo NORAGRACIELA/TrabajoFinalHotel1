1df5759501e9479654643283d4c1ccddc0279343
201 <201@academicos.cesde.edu.co>
2022-09-19 20:38:47 -0500

    progreso

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -12,6 +12,49 @@ export function Home (){
                 </div>
                 <div className='col-12 col-md-8'>
                     <h1>HISTORIA</h1>
+                    <h5>En 1829, el arquitecto Isaiah Rogers
+                    emprendió la construcción de un gran hotel en
+                    Boston. La Tremont House, fue el primer hotel de primera clase y
+                    el hito hacia la revolución en la hospitalidad. Fue el
+                    primer hotel que ofreció habitaciones con llave. Cada
+                    habitación tenía un lavabo, una jarra y una barra
+                    de jabón. Otra de sus innovaciones fue emplear personal de
+                    tiempo completo. Tremont House fue la precursora de una nueva
+                    generación de prestigiosos establecimientos de
+                    hospedaje.
+
+                    En 1874, en San Francisco, Ralston, se dio
+                    a la tarea de construir el hotel más lujoso del mundo. Su
+                    grandiosa creación, el Palace Hotel, ostentaba 800
+                    habitaciones y se erguía a la altura de 7 pisos. Este
+                    hotel se convirtió en un símbolo de la
+                    transición de San Francisco: de un pueblo floreciente a
+                    una prominente ciudad de estatura internacional. El Palace
+                    tenía su propia planta eléctrica, un abastecimiento
+                    de agua proveniente de pozos profundos, extinguidores de fuego y
+                    aire acondicionado.
+
+                    El Palace y los hoteles de primera clase
+                    que le siguieron atraían a la gente extremadamente rica y,
+                    como en los hoteles europeos de su clase, estaban más
+                    allá de los recursos del ciudadano medio.</h5>
+
+                    <h3>VER FOTOS</h3>
+                    <div className='row'>
+                    <div className='col-12 col-md-8'>
+                        <img className= "w-90 h-90 w-fluid" src='https://firebasestorage.googleapis.com/v0/b/frontcomfamangh.appspot.com/o/habitacio4.jpg?alt=media&token=32930c88-609d-41c5-b8ac-d4104e54a6b0'>
+                        </img>
+                    </div>
+                    </div>
+                    <div className='col-12 col-md-8'>
+                        <img className= "w-80 h-80 w-fluid" src='https://firebasestorage.googleapis.com/v0/b/frontcomfamangh.appspot.com/o/HABITACION-halHD2.jpg?alt=media&token=c65e4f46-89b5-4cd2-9dad-f037ab466652'>
+                        </img>
+                    </div>
+                    <div className='col-12 col-md-8'>
+                        <img className= "w-90 h-90 w-fluid" src='https://firebasestorage.googleapis.com/v0/b/frontcomfamangh.appspot.com/o/HABITACION-halHD3.jpg?alt=media&token=72866c58-5da5-4869-a50a-41722762eea4'>
+                        </img>
+                    </div>
+                    
                 </div>
             </div>
         </div>
